0281a884f56b9723d25ec3dcd84bc931e3ad22e9
ErsinAscar <Ersin.ascar@gmail.com>
2022-11-16 22:56:29 +0600

    add bootstrap and extend

--- find_route/urls.py
+++ find_route/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
-from find_route.views import home
+from find_route.views import home, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home', home),
+    path('about/', about),
+
 ]
