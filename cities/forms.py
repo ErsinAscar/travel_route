25693557ddc365272620f75094bd05f3eebcc1d9
ErsinAscar <Ersin.ascar@gmail.com>
2022-11-17 01:15:42 +0600

    Создали Update_View

--- cities/forms.py
+++ cities/forms.py
@@ -7,6 +7,10 @@ class HtmlForm(forms.Form):
     name = forms.CharField(label='Город')
 
 class CityForm(forms.ModelForm):
+    name = forms.CharField(label='Город', widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Введите название города'
+    }))
 
     class Meta:
         model = City
